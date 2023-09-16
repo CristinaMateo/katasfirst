651d65bbf35a244cebee5ab2074c838601793a7b
Cristina Mateo <cris.mateo.99@gmail.com>
2023-09-16 19:07:58 +0200

    remove consoloe.log

--- Katas.js
+++ Katas.js
@@ -1,17 +1,11 @@
 function isPalindrome(string) {
     let weTried = Array.from(string);
     let test = weTried.reverse();
-  
-  console.log(weTried)
-  console.log(test)
-    
+ 
     if (weTried === test){
       return weTried, true
     } else {
       return false
     }
   }
-  
-  let string ='hello'
-
-  
+  console.log(isPalindrome("patata"))
